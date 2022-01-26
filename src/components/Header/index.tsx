05d5d4391393794f2477a6d60f36328c782b4f3c
Lailson Sobral <lailsonlm@hotmail.com>
2022-01-26 20:08:00 -0300

    use component image

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import styles from './header.module.scss'
 
 export default function Header(): JSX.Element {
@@ -6,7 +7,7 @@ export default function Header(): JSX.Element {
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <Link href="/">
-          <img src="/logo.svg" alt="logo" />
+          <Image src="/logo.svg" alt="Logo Space Traveling" width="239px" height="26px"/>
         </Link>
       </div>
   </header>
